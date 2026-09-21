f1e6fe32b305b1d7d83fc2deb413242cb0771db0
a804 <arthur804@gmail.com>
2026-09-21 12:52:32 +0200

    Improved byte storage (with minimal loss of speed)

--- src/qtnotes/spellcheck/Compression.java
+++ src/qtnotes/spellcheck/Compression.java
@@ -5,22 +5,22 @@ import java.io.RandomAccessFile;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
-import java.util.BitSet;
-import java.util.Locale;
+import java.util.*;
 
 public final class Compression {
 
     public static final int shiftMount = 96;
     public static final int charPerChar = 5;
-    public static final int charPerCharCheck = 15;
-    public static final int lineSize = 28;
+    public static final int byteInByteArray = 4;
+    public static final int clusterSize = 6;
+    public static final int maxSize = 6*8;
 
     public static String path;
 
     public static void add(String value) {
         try {
-            var searchValue = Compression.textToLetterCompress(value).toByteArray();
+            var searchValue = Compression.textToLetterCompressOther(value);
+            if (searchValue == null) return;
             var start = find(value);
             var pathUsed = Path.of(path);
             var text = Files.readAllBytes(pathUsed);
@@ -31,7 +31,8 @@ public final class Compression {
             var secondHalf = Arrays.copyOfRange(text, -start, text.length);
 
             Files.write(pathUsed, firstHalf);
-            Files.write(pathUsed, searchValue, StandardOpenOption.APPEND);
+            for (var x : searchValue)
+                Files.write(pathUsed, x, StandardOpenOption.APPEND);
             Files.write(pathUsed, secondHalf, StandardOpenOption.APPEND);
         } catch (IOException ignored) {
 
@@ -40,7 +41,8 @@ public final class Compression {
 
     public static void remove(String value) {
         try {
-            var searchValue = Compression.textToLetterCompress(value).toByteArray();
+            var searchValue = Compression.textToLetterCompressOther(value);
+            if (searchValue == null) return;
             var start = find(value);
             var pathUsed = Path.of(path);
             var text = Files.readAllBytes(pathUsed);
@@ -49,7 +51,7 @@ public final class Compression {
                 return;
 
             var firstHalf = Arrays.copyOfRange(text, 0, start);
-            var secondHalf = Arrays.copyOfRange(text, start + searchValue.length, text.length);
+            var secondHalf = Arrays.copyOfRange(text, start + searchValue.size()*byteInByteArray, text.length);
 
             Files.write(pathUsed, firstHalf);
             Files.write(pathUsed, secondHalf, StandardOpenOption.APPEND);
@@ -59,100 +61,199 @@ public final class Compression {
     }
 
     public static int find(String value) throws IOException {
-        var searchValue = Compression.textToLetterCompress(value);
+        var searchValue = Compression.textToLetterCompressOther(value);
+        if (searchValue == null) return Integer.MIN_VALUE;
         // open the file for reading
         RandomAccessFile raf = new RandomAccessFile(path, "r");
-        var byteSizeLine = searchValue.toByteArray().length;
-        int numberOfLines = Math.toIntExact(raf.length() / byteSizeLine);
+        //5 letters of 5 bits + 1 bit
 
         // perform the binary search...
-        byte[] lineBuffer = new byte[byteSizeLine];
         int bottom = 0;
-        int top = numberOfLines;
+        int top = Math.toIntExact((raf.length() - 1) / byteInByteArray);
         int middle = 0;
+        var bytes = new ArrayList<byte[]>(8);
+        for (int i = 0; i < 8; i++) {
+            bytes.add(new byte[searchValue.get(0).length]);
+        }
+        var bytesOut = new ArrayList<byte[]>(byteInByteArray);
         while (bottom <= top) {
             middle = (bottom + top) / 2;
-            raf.seek((long) middle * byteSizeLine); // jump to this line in the file
-            raf.read(lineBuffer); // read the line from the file
-            var line = BitSet.valueOf(lineBuffer); // convert the line to a String
+            raf.seek((long) middle * byteInByteArray); // jump to this line in the file
+            raf.read(bytes.get(0)); // read the line from the file
+            bytesOut.add(bytes.get(0));
 
-            int comparison = compare(line, searchValue);
-//            System.out.println(Compression.textFromLetterCompress(line) + "->" + comparison);
+            int mountDown = 1;
+            int counter = 1;
+            while (Compression.needPreviousCluster(bytesOut.get(0))){
+                var seek = middle * byteInByteArray - (byteInByteArray * mountDown);
+                raf.seek(seek);
+                raf.read(bytes.get(counter));
+                bytesOut.add(0, bytes.get(counter));
+                mountDown ++;
+                counter++;
+            }
+            int mountUp = 1;
+            while (Compression.needNextCluster(bytesOut.get(bytesOut.size()-1))){
+                var seek = middle * byteInByteArray + (byteInByteArray * mountUp);
+                raf.seek(seek);
+                raf.read(bytes.get(counter));
+                bytesOut.add(bytes.get(counter));
+                mountUp ++;
+                counter++;
+            }
+//            System.out.print(Compression.textFromLetterCompressCluster(bytesOut.get(0)));
+//            if (bytesOut.size() > 1)
+//                System.out.println("|" + Compression.textFromLetterCompressCluster(bytesOut.get(1)));
+//            else
+//                System.out.print('\n');
+
+            int comparison = compare(bytesOut, searchValue);
+
+            bytesOut.clear();
+
             if (comparison == 0) {
                 raf.close();
-                return middle * byteSizeLine;
+                return middle * byteInByteArray;
             } else if (comparison < 0) {
                 // line comes before searchValue
-                bottom = middle + 1;
+                bottom = middle + mountUp;
             } else {
                 // line comes after searchValue
-                top = middle - 1;
+                top = middle - mountDown;
             }
         }
 
         raf.close();
-        return -middle * byteSizeLine;
+        return -middle * byteInByteArray;
     }
 
-    public static BitSet textToLetterCompress(String line) {
+    public static ArrayList<byte[]> textToLetterCompressOther(String line) {
+//        th, he, in, en, nt
         //should have been done before but we programming defensively now
         line = line.toLowerCase(Locale.ROOT);
-        if (line.length() < 3 || line.length() > lineSize)
-            return new BitSet();
-        var myBits = new BitSet(lineSize);
-        for (int i = 0; i < lineSize; i++) {
-            if (i >= line.length()) {
-                myBits.set(i * charPerChar, lineSize * charPerChar, true);
-                break;
-            }
-            int next = line.charAt(i) - shiftMount;
+        if (line.length() < byteInByteArray)
+            return null;
 
-            for (int ii = 0; ii < charPerChar; ii++) {
-                myBits.set(i * charPerChar + charPerChar - 1 - ii, ((next >> (ii)) % 2) == 1);
-            }
-
-        }
-        return myBits;
-    }
-
-    public static String textFromLetterCompress(BitSet in) {
-        StringBuilder out = new StringBuilder();
-        //get 4 bites
-        for (int i = 0; i < lineSize; i++) {
-            int calc = 0;
-            for (int ii = 0; ii < charPerChar; ii++) {
-                if (in.get(i * charPerChar + ii)) {
-                    calc += 1 << (charPerChar - 1 - ii);
+        var list = new ArrayList<byte[]>();
+        var myBits = new BitSet(31);
+        int counter = 0;
+        int letters = 0;
+        int totalCounter = 0;
+        var listArray = line.toCharArray();
+        for (int i = 0; i < listArray.length; i++) {
+            totalCounter++;
+            int calc = listArray[i];
+            if (i != listArray.length - 1){
+                var tempCalc = indexAf(listArray, i);
+                if (tempCalc != 0) {
+                    calc = tempCalc;
+                    i++;
+                    totalCounter++;
                 }
             }
-            if (calc == 31)
+            int next = calc - shiftMount;
+            for (int ii = 0; ii < charPerChar; ii++) {
+                myBits.set(counter, ((next >> (ii)) % 2) == 1);
+                counter ++;
+            }
+            if (letters == clusterSize - 1){
+                myBits.set(30, !list.isEmpty());
+                myBits.set(31, totalCounter < line.length());
+                list.add(myBits.toByteArray());
+                myBits = new BitSet(31);
+                letters = counter = 0;
+            } else
+                letters ++;
+        }
+        if (!myBits.isEmpty()) {
+            myBits.set(30, !list.isEmpty());
+            myBits.set(31, false);
+            var listo = new byte[byteInByteArray];
+            var tryList = myBits.toByteArray();
+            listo[0] = tryList[0];
+            listo[1] = tryList.length > 1 ? tryList[1] : 0;
+            listo[2] = tryList.length > 2 ? tryList[2] : 0;
+            listo[3] = tryList.length > 3 ? tryList[3] : 0;
+            list.add(listo);
+        }
+        return list;
+    }
+
+    private static int indexAf(char[] listArray, int i) {
+        return switch ("" + listArray[i] + listArray[i + 1]) {
+            case "th" -> 122;
+            case "he" -> 123;
+            case "in" -> 124;
+            case "ten" -> 125;
+            case "nt" -> 126;
+            case "re" -> 127;
+            default -> 0;
+        };
+    }
+
+    private static String indexAf(int i) {
+        return switch (i) {
+            case 122 -> "th";
+            case 123 -> "he";
+            case 124 -> "in";
+            case 125 -> "ten";
+            case 126 -> "nt";
+            case 127 -> "re";
+            default -> Character.toString(i);
+        };
+    }
+
+    //without first bit
+    public static String textFromLetterCompressCluster(byte[] inB) {
+        var in = BitSet.valueOf(inB);
+        StringBuilder out = new StringBuilder();
+        //get 4 bites
+        for (int i = 0; i < clusterSize; i++) {
+            int calc = 0;
+            for (int ii = 0; ii < charPerChar; ii++) {
+                if (in.get((i * charPerChar + ii))) {
+                    calc += 1 << (ii);
+                }
+            }
+            if (calc == 0)
                 return out.toString();
-            out.append(Character.toString(calc + shiftMount));
+            out.append(indexAf(calc + shiftMount));
         }
         return out.toString();
     }
 
-    public static int compare(BitSet b1, BitSet b2) {
-        int N = b1.length();
-        for (int i = 0; i < N; i++) {
-            if (i % charPerChar == 0 && i > charPerCharCheck) {
-                var b1T = b1.get(i, i + charPerChar);
-                var b2T = b2.get(i, i + charPerChar);
-                if (b1T.cardinality() == charPerChar){
-                    return b2T.cardinality() == charPerChar ? 0 : -1;
-                } else if (b2T.cardinality() == charPerChar){
-                    return 1;
-                }
-            }
-            if (b1.get(i)) {
-                if (!b2.get(i)) {
-                    return 1;
-                }
-            } else if (b2.get(i)) {
-                return -1;
+    public static boolean needNextCluster(byte[] in) {
+        return in[3] < 0;
+    }
+    public static boolean needPreviousCluster(byte[] in) {
+        return in[3] > 63 || (in[3] > -65 && in[3] < 0);
+    }
+
+    public static int compare(List<byte[]> line, List<byte[]> search) {
+        for (int i = 0; i < line.size(); i++) {
+            if (search.size() == i) return 1;
+            var a = line.get(i);
+            var b = search.get(i);
+
+            int ina = mismatch(a, b,
+                    b.length);
+            if (ina >= 0) {
+                return Byte.compare(a[ina], b[ina]);
             }
         }
+        if (line.size() < search.size())
+            return -1;
         return 0;
+    }
+
+    public static int mismatch(byte[] a,
+                               byte[] b,
+                               int length) {
+        for (int i = 0; i < length; i++) {
+            if (a[i] != b[i])
+                return i;
+        }
+        return -1;
     }
 }
 
--- src/test/TestSpellCheck.java
+++ src/test/TestSpellCheck.java
@@ -14,90 +14,107 @@ import java.util.*;
 //
 public class TestSpellCheck {
     String fileLocation = "/home/a804/Documents/MyTyper/words.txt";
-    String fileLocation3 = "/home/a804/Documents/MyTyper/words_bin.dat";
-    String testWord = "managed";
+    String fileLocation4 = "/home/a804/Documents/MyTyper/words_bin.dat";
+    String testWord = "ccesser";
 
     @Test
     public void removeCheck() throws IOException {
-        Compression.path = fileLocation3;
-        fixBinaryFile();
-        var length = Files.readAllBytes(Path.of(fileLocation3)).length;
+        Compression.path = fileLocation4;
+        fixNextBinaryFile();
+        var length = Files.readAllBytes(Path.of(fileLocation4)).length;
         Assert.assertTrue(Compression.find(testWord) != -1);
         Compression.remove(testWord);
         Assert.assertTrue(0 > Compression.find(testWord));
-        Assert.assertEquals(length, Files.readAllBytes(Path.of(fileLocation3)).length + Compression.textToLetterCompress(testWord).toByteArray().length);
-        fixBinaryFile();
-
+        Assert.assertEquals(length, Files.readAllBytes(Path.of(fileLocation4)).length + Compression.textToLetterCompressOther(testWord).size()* 4L);
+        fixNextBinaryFile();
     }
 
     @Test
     public void addCheck() throws IOException, InterruptedException {
-        Compression.path = fileLocation3;
-        fixBinaryFile();
-        var length = Files.readAllBytes(Path.of(fileLocation3)).length;
-        Assert.assertTrue(0 > Compression.find(testWord+testWord));
-        Compression.add(testWord+testWord);
-        Assert.assertEquals(length, Files.readAllBytes(Path.of(fileLocation3)).length - Compression.textToLetterCompress(testWord).toByteArray().length);
-        Assert.assertTrue(Compression.find(testWord+testWord) != -1);
-        Compression.remove(testWord+testWord);
-        Assert.assertEquals(length, Files.readAllBytes(Path.of(fileLocation3)).length);
+        Compression.path = fileLocation4;
+        fixNextBinaryFile();
+        var length = Files.readAllBytes(Path.of(fileLocation4)).length;
+        Assert.assertTrue(0 > Compression.find(testWord + testWord));
+        Compression.add(testWord + testWord);
+        Assert.assertTrue(Compression.find(testWord + testWord) > -1);
+        Compression.remove(testWord + testWord);
+        Assert.assertEquals(length, Files.readAllBytes(Path.of(fileLocation4)).length);
     }
+
     @Test
     public void searchTest() throws IOException {
 //        var start = System.currentTimeMillis();
-        Compression.path = fileLocation3;
-        var found = Compression.find("strawberry");
+        Compression.path = fileLocation4;
+        var found = Compression.find("probably");
 //        System.out.println(System.currentTimeMillis() - start);
         Assert.assertTrue(found > -1);
     }
 
     @Test
-    public void fixBinaryFile() throws IOException {
+    public void searchTest2() throws IOException {
+//        var start = System.currentTimeMillis();
+        Compression.path = fileLocation4;
+        var found = Compression.find("accreted");
+//        System.out.println(System.currentTimeMillis() - start);
+        Assert.assertTrue(found > -1);
+    }
+
+    @Test
+    public void fixNextBinaryFile() throws IOException {
         Path path = Paths.get(fileLocation);
-        Path outPath = Path.of(fileLocation3);
+        Path outPath = Path.of(fileLocation4);
         List<String> lines = Files.readAllLines(path);
         lines.sort(String::compareTo);
-        lines = lines.stream().filter(x -> x.length() > 3 && x.length() < Compression.lineSize).toList();
+        lines = lines.stream().filter(x -> x.length() > 3).toList();
         Files.writeString(path, String.join("\n", lines));
-        var list = lines.stream().filter(e -> e.length() > 3 && e.length() < Compression.lineSize).map((x) -> Compression.textToLetterCompress(x).toByteArray()).sorted((a,b) -> Compression.compare(BitSet.valueOf(a),BitSet.valueOf(b))).toList();
+        var list = lines.stream()
+                .filter(e -> e.length() > 3 && e.length() < Compression.maxSize)
+                .map(Compression::textToLetterCompressOther)
+                .filter(Objects::nonNull)
+                .sorted(Compression::compare).toList();
 
         Files.deleteIfExists(outPath);
         Files.createFile(outPath);
-        for (var in : list){
-            Files.write(outPath, in, StandardOpenOption.APPEND);
+        for (var in : list) {
+            for (var inIn : in)
+                Files.write(outPath, inIn, StandardOpenOption.APPEND);
         }
     }
 
     @Test
     public void readTest() throws IOException {
-        Compression.textToLetterCompress("aaa");
-
-        BitSet readBits;
-        try (DataInputStream in = new DataInputStream(new FileInputStream(fileLocation3))) {
-            byte[] byteArray = new byte[15]; // Adjust size accordingly
-            in.readFully(byteArray);
-            readBits = BitSet.valueOf(byteArray);
+        var x = Files.readAllBytes(Path.of(fileLocation4));
+        for (int i = 0; i < 500; i += 4) {
+            var list = new byte[4];
+            list[0] = x[i];
+            list[1] = x[1+ i];
+            list[2] = x[2+ i];
+            list[3] = x[3 + i];
+            System.out.println(Compression.textFromLetterCompressCluster(list) + " " + Compression.needNextCluster(list) + " " + Compression.needPreviousCluster(list));
         }
-        var y = Compression.textFromLetterCompress(readBits);
-        var z= 0;
     }
 
     @Test
-    public void checkCompressionSingleWord() throws IOException {
-//        assertEquals(testWord, Compression.textFromLetterCompress(Compression.padAndFilter(testWord)));
+    public void readTestTwo() throws IOException {
+        var arst = new BitSet();
+//        arst.set(31);
+        arst.set(30);
+        var arf = arst.toByteArray();
+        var press = Compression.textToLetterCompressOther(testWord);
+        var press1 = Compression.textToLetterCompressOther("bbbbbbbbbbbbb");
+        var press2 = Compression.textToLetterCompressOther("zzzzzzzzzzzzz");
+        var press3 = Compression.textToLetterCompressOther("aahing");
 
-        Compression.path = fileLocation3;
-        var word = testWord;
-        if (!(Compression.find(word) > -1))
-            Assert.fail();
-
+        var dePress = Compression.textFromLetterCompressCluster(press.get(0));
+        var goLeft = Compression.needPreviousCluster(press.get(1));
+        var goRight = Compression.needNextCluster(press.get(1));
+        BitSet readBits;
+        var z = 0;
     }
 
     @Test
     public void checkCompression() throws IOException {
-//        assertEquals(testWord, Compression.textFromLetterCompress(Compression.padAndFilter(testWord)));
-
-        Compression.path = fileLocation3;
+        Compression.path = fileLocation4;
         Path outPath = Path.of(fileLocation);
         var lines = Files.readAllLines(outPath);
         List<String> fail = new ArrayList<>(5000);
@@ -111,12 +128,11 @@ public class TestSpellCheck {
 
     @Test
     public void createSuggestions() throws InterruptedException {
-        Compression.path = fileLocation3;
+        Compression.path = fileLocation4;
         var start = System.currentTimeMillis();
 
         var x = Spellcheck.optimizedSearch(testWord);
         System.out.println(System.currentTimeMillis() - start);
-
 
         var y = 0;
     }
